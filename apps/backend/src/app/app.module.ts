80367db68b7d25f0c9db8a146dd0ccf2f680fb3d
James Colesanti <jamescolesanti@.yahoo.com>
2023-10-28 20:36:00 -0400

    GI: Commented out database import

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -7,19 +7,19 @@ import { PluralNamingStrategy } from '../strategies/plural-naming.strategy';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mongodb',
-      host: '127.0.0.1',
-      port: 27017,
-      database: 'scaffoldingTest',
-      // username: 'root',
-      // password: 'root',
-      autoLoadEntities: true,
-      // entities: [join(__dirname, '**/**.entity.{ts,js}')],
-      // Setting synchronize: true shouldn't be used in production - otherwise you can lose production data
-      synchronize: true,
-      namingStrategy: new PluralNamingStrategy(),
-    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'mongodb',
+    //   host: '127.0.0.1',
+    //   port: 27017,
+    //   database: 'scaffoldingTest',
+    //   // username: 'root',
+    //   // password: 'root',
+    //   autoLoadEntities: true,
+    //   // entities: [join(__dirname, '**/**.entity.{ts,js}')],
+    //   // Setting synchronize: true shouldn't be used in production - otherwise you can lose production data
+    //   synchronize: true,
+    //   namingStrategy: new PluralNamingStrategy(),
+    // }),
   ],
   controllers: [AppController],
   providers: [AppService],
