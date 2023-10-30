f392a7a769a3ef942c548ff21d5ec57ca008fbc8
Kenny Jung <33704204+chromium-52@users.noreply.github.com>
2023-10-30 17:28:23 +0900

    Trigger backend redeploy (#24)

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,7 +6,6 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
