18b83b66cbbe2dfbeeb3f1592898d51b90786633
jarenadams21 <jarenadams3@gmail.com>
2024-03-18 18:40:57 -0400

    header centered

--- apps/frontend/src/components/volunteer/signup/SignUpPage.tsx
+++ apps/frontend/src/components/volunteer/signup/SignUpPage.tsx
@@ -1,9 +1,12 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Text } from '@chakra-ui/react';
 
 export default function SignUpPage() {
     return (
         <Box bg='tomato' w='1104px' h='1025px' top='192px' left='89px' border='1px'>
-            <button>hu</button>
+            <Box className='header-and-line' display='flex' justifyContent='center'>
+                <Text fontStyle='Montserrat' fontSize='30px'>Welcome, Volunteer!</Text>
+                <button> This will be a line </button>
+            </Box>
         </Box>
     )
 }
