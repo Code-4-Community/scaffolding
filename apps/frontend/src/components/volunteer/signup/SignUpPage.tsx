ca799617ca40215c54b90e424dcc693456238b1c
jarenadams21 <jarenadams3@gmail.com>
2024-03-18 18:29:56 -0400

    exact styling

--- apps/frontend/src/components/volunteer/signup/SignUpPage.tsx
+++ apps/frontend/src/components/volunteer/signup/SignUpPage.tsx
@@ -2,7 +2,7 @@ import { Box } from '@chakra-ui/react';
 
 export default function SignUpPage() {
     return (
-        <Box bg='tomato' w='1104px' h='1025px'>
+        <Box bg='tomato' w='1104px' h='1025px' top='192px' left='89px' border='1px'>
             <button>hu</button>
         </Box>
     )
