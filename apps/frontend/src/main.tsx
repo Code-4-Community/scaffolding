a50e96ff1e9c26540a87c1b3dd7453e46ab5f85e
jarenadams21 <jarenadams3@gmail.com>
2024-04-18 12:30:32 -0400

    route updates and volunteer links to my gi page

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MapPage from './pages/mapPage/MapPage';
+import MyAdoptedGIPage from './pages/myAdoptedGIPage/MyAdoptedGIPage';
+import VolunteerPage from './pages/volunteerPage/VolunteerPage';
 
 const queryClient = new QueryClient();
 
@@ -10,9 +12,11 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MapPage />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<MapPage />} />
+        <Route path="/volunteer" element={<VolunteerPage />} />
+        <Route path="/volunteer/my-adopted-gi" element={<MyAdoptedGIPage />} />
+      </Routes>
       </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>,
