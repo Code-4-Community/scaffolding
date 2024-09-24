d40c67f86a326e8303c203038ded3d2c096e72ae
ItsEricSun <ericsun828@gmail.com>
2024-09-23 21:59:19 -0400

    remove unused import

--- apps/frontend/src/components/volunteerDashboard/MaintenanceChecklistPopup.tsx
+++ apps/frontend/src/components/volunteerDashboard/MaintenanceChecklistPopup.tsx
@@ -6,7 +6,6 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import { useState } from 'react';
 import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
