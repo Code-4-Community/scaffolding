76b1d636bec5f2651a61b8e7e2801102bd1409ee
Olivier John Nzia <116421013+ojn03@users.noreply.github.com>
2024-10-19 09:23:24 -0700

    include associated user relation to applications query (#70)

--- apps/backend/src/applications/applications.service.ts
+++ apps/backend/src/applications/applications.service.ts
@@ -140,7 +140,7 @@ export class ApplicationsService {
         year: getCurrentYear(),
         semester: getCurrentSemester(),
       },
-      relations: ['reviews'],
+      relations: ['user'],
     });
 
     const allApplicationsDto = applications.map((app) => {
