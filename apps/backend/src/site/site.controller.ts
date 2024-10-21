29475dc285a34d91f7e2f075349c4acd812b3ee8
nourshoreibah <nour17372@icloud.com>
2024-10-21 19:58:40 -0400

    Refactor sites status and symbol into 2 endpoints

--- apps/backend/src/site/site.controller.ts
+++ apps/backend/src/site/site.controller.ts
@@ -2,7 +2,8 @@ import {
     Controller,
     Get,
     Param,
-    Query
+    Query,
+    Delete
 } from "@nestjs/common";
 import { SiteService } from "./site.service";
 import { SiteModel } from "./site.model";
@@ -12,6 +13,26 @@ import { ApiQuery } from "@nestjs/swagger";
 export class SiteController {
     constructor(private siteService: SiteService) {}
 
+    @Get("/status/")
+    @ApiQuery({ name: "status", required: true })
+    public async getSitesByStatus(
+        @Query('status') status: string        
+    ): Promise<SiteModel[]> {
+        console.log("status: ", status);
+        return this.siteService.getSitesByStatus(status);
+    }
+
+
+    
+    @Get("/symbolType/")
+    @ApiQuery({ name: "symbolType", required: true })
+    public async getSitesBySymbolType(
+        @Query('symbolType') symbolType: string
+    ): Promise<SiteModel[]> {
+        return this.siteService.getSitesBySymbolType(symbolType);
+    }
+
+
     @Get(":id")
     public async getSite(
         @Param("id") siteId: number
@@ -19,15 +40,10 @@ export class SiteController {
         return this.siteService.getSite(siteId);
     }  
 
-    @Get()
-    @ApiQuery({ name: 'status', required: false }) // makes query parameter optional
-    @ApiQuery({ name: 'symbol-type', required: false })
-    public async getSites(
-        @Query("status") status?: string,
-        @Query("symbol-type") symbolType?: string
-    ): Promise<SiteModel[]> {
-        return this.siteService.getFilteredSites({ status, symbolType });
-    }
+ 
+
+
+
 
 
 }
