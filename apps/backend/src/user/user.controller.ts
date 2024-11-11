7939ffcbe6e440e7d7d9538691ec3aa7e190f2f0
neetidesai <97416423+neetidesai@users.noreply.github.com>
2024-11-11 16:30:18 -0500

    Add post user - admin endpoint

--- apps/backend/src/user/user.controller.ts
+++ apps/backend/src/user/user.controller.ts
@@ -1,37 +1,32 @@
-import {
-    Controller,
-    Get,
-    Post,
-    Body,
-    Param,
-} from "@nestjs/common";
-import { UserService } from "./user.service";
-import { UserModel } from "./user.model";
-import { NewUserInput } from "../dtos/newUserDTO";
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserModel, Role } from './user.model';
+import { NewUserInput } from '../dtos/newUserDTO';
 
 /**
  * The controller for user endpoints.
  */
-@Controller("users")
+@Controller('users')
 export class UserController {
-    constructor(private userService: UserService) {}
+  constructor(private userService: UserService) {}
 
-    @Get(":id")
-    public async getUser(
-        @Param("id") userId?: number
-        ): Promise<UserModel> {
-            return this.userService.getUser(userId);
-        }
+  @Get(':id')
+  public async getUser(@Param('id') userId?: number): Promise<UserModel> {
+    return this.userService.getUser(userId);
+  }
 
-    @Post("/addVolunteer")
-    public async addVolunteer( @Body() userData: NewUserInput) {
-        return this.userService.postUserVolunteer(userData);
-    }
-    
-    @Get(":id/sites")
-    public async getUserSites(@Param("id") userId?: number): Promise<any> {
-        return this.userService.getUserTables(userId);
-    }
+  @Post('addVolunteer')
+  public async addVolunteer(@Body() userData: NewUserInput) {
+    return this.userService.postUser(userData, Role.VOLUNTEER);
+  }
 
+  @Post('addAdmin')
+  public async addAdmin(@Body() userData: NewUserInput) {
+    return this.userService.postUser(userData, Role.ADMIN);
+  }
 
+  @Get(':id/sites')
+  public async getUserSites(@Param('id') userId?: number): Promise<any> {
+    return this.userService.getUserTables(userId);
+  }
 }
--- apps/backend/src/user/user.service.ts
+++ apps/backend/src/user/user.service.ts
@@ -1,111 +1,113 @@
-import { Injectable } from "@nestjs/common";
-import { UserModel } from "./user.model";
-import { DynamoDbService } from "../dynamodb";
-import { UserInputModel, UserStatus, Role } from "./user.model";
-import { NewUserInput } from "../dtos/newUserDTO";
+import { Injectable } from '@nestjs/common';
+import { UserModel } from './user.model';
+import { DynamoDbService } from '../dynamodb';
+import { UserInputModel, UserStatus, Role } from './user.model';
+import { NewUserInput } from '../dtos/newUserDTO';
 
 @Injectable()
 export class UserService {
+  private readonly tableName = 'gibostonUsers';
+  constructor(private readonly dynamoDbService: DynamoDbService) {}
 
-    private readonly tableName = 'gibostonUsers';
-    constructor(private readonly dynamoDbService: DynamoDbService) {}
-
- 
-
-    /**
-     * Gets a user's information based on the user's id.
-     * @param userId The user's id
-     * @throws Error if the user's data could not be fetched from DynamoDB
-     * @throws Error if the user's status or role is invalid
-     * @returns The user's information as a UserModel object
-     */
-    public async getUser(userId: number): Promise<UserModel> {
-        try {
-            const key = { 'userId' : {N: userId.toString()}};
-            const data = await this.dynamoDbService.getItem(this.tableName, key);
-            if (!data) {
-                throw new Error(`No user found with id: ${userId}`);
-            }
-            console.log(data);
-            return(this.mapDynamoDBItemToUserModel(userId,data));
-
-        }
-        catch(e) {
-            throw new Error(`Error fetching data for user with id: ${userId}: ${e.message}`);
-        }
-
+  /**
+   * Gets a user's information based on the user's id.
+   * @param userId The user's id
+   * @throws Error if the user's data could not be fetched from DynamoDB
+   * @throws Error if the user's status or role is invalid
+   * @returns The user's information as a UserModel object
+   */
+  public async getUser(userId: number): Promise<UserModel> {
+    try {
+      const key = { userId: { N: userId.toString() } };
+      const data = await this.dynamoDbService.getItem(this.tableName, key);
+      if (!data) {
+        throw new Error(`No user found with id: ${userId}`);
+      }
+      console.log(data);
+      return this.mapDynamoDBItemToUserModel(userId, data);
+    } catch (e) {
+      throw new Error(
+        `Error fetching data for user with id: ${userId}: ${e.message}`,
+      );
     }
+  }
 
-    public async postUserVolunteer(userData: NewUserInput) {
-        const userModel = this.PostInputToUserVolunteerModel(userData);
-        const newId = await this.dynamoDbService.getHighestUserId(this.tableName) + 1;
-        userModel.userId.N = newId.toString();
-        console.log("Using new ID:" + userModel.userId.N);
-        try {
-            const result = await this.dynamoDbService.postItem(this.tableName, userModel);
-            return {...result, newUserID: newId.toString()};
-        } catch (e) {
-            throw new Error("Unable to post new user: " + e);
-        }
+  public async postUser(userData: NewUserInput, role: Role) {
+    const userModel = this.postInputToUserModel(userData, role);
+    const newId =
+      (await this.dynamoDbService.getHighestUserId(this.tableName)) + 1;
+    userModel.userId.N = newId.toString();
+    console.log('Using new ID:' + userModel.userId.N);
+    try {
+      const result = await this.dynamoDbService.postItem(
+        this.tableName,
+        userModel,
+      );
+      return { ...result, newUserID: newId.toString() };
+    } catch (e) {
+      throw new Error('Unable to post new user: ' + e);
     }
+  }
 
-
-    public async getUserTables(userId: number): Promise<Array<number>> {
-        try {
-            const key = { 'userId' : {N: userId.toString()}};
-            const data = await this.dynamoDbService.getItem(this.tableName, key);
-            if (!data) {
-                throw new Error(`No user found with id: ${userId}`);
-            }
-            console.log(data);
-            const siteIds = data["siteIds"].L.map(item => Number(item.N));
-            return siteIds;
-        } 
-        catch(e) {
-            throw new Error(`Error fetching data for user with id: ${userId}: ${e.message}`);
-        }
+  public async getUserTables(userId: number): Promise<Array<number>> {
+    try {
+      const key = { userId: { N: userId.toString() } };
+      const data = await this.dynamoDbService.getItem(this.tableName, key);
+      if (!data) {
+        throw new Error(`No user found with id: ${userId}`);
+      }
+      console.log(data);
+      const siteIds = data['siteIds'].L.map((item) => Number(item.N));
+      return siteIds;
+    } catch (e) {
+      throw new Error(
+        `Error fetching data for user with id: ${userId}: ${e.message}`,
+      );
     }
+  }
 
+  /**
+   * Maps a user's data from DynamoDB to a UserModel object.
+   * @param objectId the user's id
+   * @param data the user's data from DynamoDB
+   * @throws Error if the user's status or role is invalid
+   * @returns the user's information as a UserModel object
+   */
+  private mapDynamoDBItemToUserModel(
+    objectId: number,
+    data: { [key: string]: any },
+  ): UserModel {
+    const siteIds = data['siteIds']?.NS?.map(Number) ?? [];
 
-    /**
-     * Maps a user's data from DynamoDB to a UserModel object.
-     * @param objectId the user's id
-     * @param data the user's data from DynamoDB
-     * @throws Error if the user's status or role is invalid
-     * @returns the user's information as a UserModel object
-     */
-    private mapDynamoDBItemToUserModel(objectId: number, data: {[key: string]: any}): UserModel {
+    return {
+      userId: objectId,
+      firstName: data['firstName'].S,
+      lastName: data['lastName'].S,
+      email: data['email'].S,
+      phoneNumber: data['phoneNumber'].N,
+      siteIds: siteIds,
+      zipCode: data['zipCode'].S,
+      birthDate: new Date(data['birthDate'].S),
+      role: data['role'].S,
+      status: data['status'].S,
+    };
+  }
 
-        const siteIds = data["siteIds"]?.NS?.map(Number) ?? [];
-
-
-        return {
-            userId: objectId,
-            firstName: data['firstName'].S,
-            lastName: data['lastName'].S,
-            email: data['email'].S,
-            phoneNumber: data['phoneNumber'].N,
-            siteIds: siteIds,
-            zipCode: data['zipCode'].S,
-            birthDate: new Date(data['birthDate'].S),
-            role: data['role'].S,
-            status: data['status'].S
-        };
-    }
-
-    private PostInputToUserVolunteerModel = (input: NewUserInput): UserInputModel => {
-        return {
-            userId: {N: "0"},
-            firstName: {S: input.firstName},
-            lastName: {S: input.lastName},
-            phoneNumber: {S: input.phoneNumber},
-            email: {S: input.email},
-            zipCode: {S: input.zipCode},
-            birthDate: {S: input.birthDate},
-            role: {S: Role.VOLUNTEER},
-            siteIds: {S: "null"},
-            status: {S: UserStatus.PENDING}
-        };
-    }
-
+  private postInputToUserModel = (
+    input: NewUserInput,
+    role: Role,
+  ): UserInputModel => {
+    return {
+      userId: { N: '0' },
+      firstName: { S: input.firstName },
+      lastName: { S: input.lastName },
+      phoneNumber: { S: input.phoneNumber },
+      email: { S: input.email },
+      zipCode: { S: input.zipCode },
+      birthDate: { S: input.birthDate },
+      role: { S: role },
+      siteIds: { S: 'null' },
+      status: { S: UserStatus.PENDING },
+    };
+  };
 }
