e5d3db271736678d370fd103896cc2b4c9be230d
nourshoreibah <nour17372@icloud.com>
2025-02-06 12:08:48 -0500

    Register lambda service

--- apps/backend/src/applications/applications.module.ts
+++ apps/backend/src/applications/applications.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { ApplicationsService } from './applications.service';
 import { ApplicationsController } from './applications.controller';
 import { DynamoDbService } from '../dynamodb';
+import { LambdaService } from '../lambda';
+import { UserService } from '../user/user.service';
+import { SiteService } from '../site/site.service';
 
 @Module({
   imports: [],
-  providers: [ApplicationsService, DynamoDbService],
+  providers: [ApplicationsService, DynamoDbService, LambdaService, UserService, SiteService],
   controllers: [ApplicationsController],
   exports: [ApplicationsService],
 })
