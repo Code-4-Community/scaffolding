8b3cc5a211a4a47918c92729ebd71adcc9731f3e
Hamsini Malli <malli.hamsini@gmail.com>
2025-02-13 15:36:54 -0500

    Update user.service.ts

--- apps/backend/src/user/user.service.ts
+++ apps/backend/src/user/user.service.ts
@@ -10,6 +10,8 @@ export class UserService {
     private readonly tableName = 'gibostonUsers';
     constructor(private readonly dynamoDbService: DynamoDbService) {}
 
+
+
     /**
      * Gets a user's information based on the user's id.
      * @param userId The user's id
