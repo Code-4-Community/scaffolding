62af0da398a0523ccbfe3e97f0257d78759eba43
Hamsini Malli <malli.hamsini@gmail.com>
2025-02-13 15:37:38 -0500

    Update user.module.ts

--- apps/backend/src/user/user.module.ts
+++ apps/backend/src/user/user.module.ts
@@ -2,8 +2,6 @@ import { Module } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { UserController } from "./user.controller";
 import { DynamoDbService } from "../dynamodb";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { User } from "./user.entity";
 
 @Module({
     providers: [UserService, DynamoDbService],
