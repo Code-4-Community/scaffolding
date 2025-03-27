4b88f8b5b4a1ac784188f387ed4a839fb1cdf7b6
pujitakalinadhabhotla <161961520+pujitakalinadhabhotla@users.noreply.github.com>
2025-03-26 22:23:47 -0400

    new changes

---
+++ apps/frontend/src/images/markers/other.tsx
@@ -0,0 +1,6 @@
+export default function generateOtherSVG(color: string) {
+    return `<svg width="41" height="40" viewBox="0 0 26 25" fill="${color}" xmlns="http://www.w3.org/2000/svg">
+      <rect x="3" y="11" width="20" height="3" fill="${color}" />
+    </svg>`;
+  }
+  
