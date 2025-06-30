5992c83569890f0298148cd91c88d5249463b271
chnnick <nickchenschool@gmail.com>
2025-06-29 23:30:39 -1000

    removing date column, rating column is last column

--- apps/frontend/src/components/ApplicationTables/columns.ts
+++ apps/frontend/src/components/ApplicationTables/columns.ts
@@ -3,21 +3,25 @@ export const applicationColumns = [
     field: 'name',
     headerName: 'Name',
     width: 150,
+    sortable: true,
   },
   {
     field: 'position',
     headerName: 'Position',
     width: 150,
+    sortable: true,
   },
   {
     field: 'reviewed',
     headerName: 'Reviewed',
     width: 150,
+    sortable: false,
   },
   {
     field: 'assignedTo',
     headerName: 'Assigned To',
     width: 150,
+    sortable: false,
   },
   {
     field: 'stage',
@@ -28,10 +32,12 @@ export const applicationColumns = [
     field: 'rating',
     headerName: 'Rating',
     width: 125,
+    sortable: false,
   },
   {
     field: 'createdAt',
     headerName: 'Date',
     width: 150,
+    sortable: false,
   },
 ];
