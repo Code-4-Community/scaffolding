270ea040f4170ebb5f6551cd57a27e814bd7f50a
Max Norman <27299284+maxn990@users.noreply.github.com>
2025-08-06 16:57:59 -0400

    update datasource
    sorry, updated this after I pushed so just gonna change it here too

--- apps/backend/src/data-source.ts
+++ apps/backend/src/data-source.ts
@@ -14,9 +14,9 @@ const AppDataSource = new DataSource({
   password: process.env.NX_DB_PASSWORD,
   database: process.env.NX_DB_DATABASE,
   entities: [Task, Label],
-  migrations: ['apps/backend/src/migrations/*.ts'],
+  migrations: ['apps/backend/src/migrations/*.js'],
   // Setting synchronize: true shouldn't be used in production - otherwise you can lose production data
-  synchronize: true,
+  synchronize: false,
   namingStrategy: new PluralNamingStrategy(),
 });
 
