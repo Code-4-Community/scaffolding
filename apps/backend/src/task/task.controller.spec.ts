bb0a0a68d670ff1f3b8a1c6c5eb64afae5e40b7b
Dalton Burkhart <daltonburkhart.75@gmail.com>
2025-08-12 16:24:34 -0400

    Completed testing

--- apps/backend/src/task/task.controller.spec.ts
+++ apps/backend/src/task/task.controller.spec.ts
@@ -1,12 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksService } from './task.service';
 import { TasksController } from './task.controller';
+import { TaskCategory } from './types/category';
+import { BadRequestException } from '@nestjs/common';
+import { Task } from './types/task.entity';
 
-// Mock implementation for Task Service
-export const mockTaskService: Partial<TasksService> = {};
+export const mockTasks = {
+  id: 1,
+  title: 'Task 1',
+  description: 'Desc 1',
+  dateCreated: new Date(),
+  dueDate: new Date(),
+  labels: [],
+  category: TaskCategory.TODO,
+};
+
+export const mockUpdatedTaskCategory: Task = {
+  ...mockTasks,
+  category: TaskCategory.IN_PROGRESS,
+};
+
+export const mockTaskService: Partial<TasksService> = {
+  updateTaskCategory: jest.fn((id: number, newCategory: TaskCategory) =>
+    Promise.resolve(mockUpdatedTaskCategory),
+  ),
+};
 
 describe('TasksController', () => {
   let controller: TasksController;
+  let tasksService: TasksService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -35,6 +57,34 @@ describe('TasksController', () => {
   /* Tests for delete task by id */
 
   /* Tests for move task category by id */
+  it('should successfully update task category when valid data is provided', async () => {
+    const taskId = mockTasks.id;
+    const newCategory = TaskCategory.IN_PROGRESS;
+
+    const result = await controller.updateTaskCategory(taskId, newCategory);
+
+    expect(result).toEqual(mockUpdatedTaskCategory);
+    expect(mockTaskService.updateTaskCategory).toHaveBeenCalledWith(
+      taskId,
+      newCategory,
+    );
+  });
+
+  it('should throw BadRequestException when task does not exist in DB', async () => {
+    const taskId = 0;
+    const newCategory = TaskCategory.COMPLETED;
+
+    (mockTaskService.updateTaskCategory as jest.Mock).mockResolvedValue(null);
+
+    await expect(
+      controller.updateTaskCategory(taskId, newCategory),
+    ).rejects.toThrow(BadRequestException);
+
+    expect(mockTaskService.updateTaskCategory).toHaveBeenCalledWith(
+      taskId,
+      newCategory,
+    );
+  });
 
   /* Tests for add labels to task by id */
 
