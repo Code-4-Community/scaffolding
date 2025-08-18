0dd655b66165cbed9ba086237b0be89a0757cfc0
Aaron <aaronashby2006@gmail.com>
2025-08-18 00:17:35 -0400

    Update update-task.dto.ts

--- apps/backend/src/task/dtos/update-task.dto.ts
+++ apps/backend/src/task/dtos/update-task.dto.ts
@@ -1,10 +1,16 @@
 import { Transform } from 'class-transformer';
+import { IsDate, IsOptional, IsString } from 'class-validator';
 
 export class UpdateTaskDTO {
+  @IsOptional()
+  @IsString()
   title?: string;
 
+  @IsOptional()
+  @IsString()
   description?: string;
 
+  @IsOptional()
   @Transform(({ value }) => new Date(value))
   dueDate?: Date;
 }
