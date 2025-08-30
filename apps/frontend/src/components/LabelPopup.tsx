df41cffb9a6c1c9d4f71b0912e55dd8473095e77
Dalton Burkhart <daltonburkhart.75@gmail.com>
2025-08-30 11:46:18 -0400

    Final commit

--- apps/frontend/src/components/LabelPopup.tsx
+++ apps/frontend/src/components/LabelPopup.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Button } from '@mui/material';
 import { MuiColorInput } from 'mui-color-input';
 
 export const LabelPopup: React.FC = () => {
@@ -7,6 +6,7 @@ export const LabelPopup: React.FC = () => {
   const [description, setDescription] = useState('');
   const [colorValue, setColorValue] = useState('#ffffff');
 
+  // TODO: button functionality
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     console.log({ name, description, colorValue });
@@ -15,9 +15,9 @@ export const LabelPopup: React.FC = () => {
   return (
     <form
       onSubmit={handleSubmit}
-      className="w-[338px] p-6 border-2 rounded-xl bg-gray-200 space-y-2 text-[28px]"
+      className="w-[338px] p-6 border-2 border-black bg-gray-100 space-y-2 text-[28px]"
     >
-      <div className="flex flex-col">
+      <div className="flex flex-col gap-2">
         <label className="font-bold">Name</label>
         <input
           type="text"
@@ -29,7 +29,7 @@ export const LabelPopup: React.FC = () => {
         />
       </div>
 
-      <div className="flex flex-col">
+      <div className="flex flex-col gap-2">
         <label className="font-bold">
           Description <span className="text-xs">(optional)</span>
         </label>
@@ -43,7 +43,7 @@ export const LabelPopup: React.FC = () => {
         />
       </div>
 
-      <div>
+      <div className="flex flex-col gap-2">
         <label className="font-bold">Color</label>
         <MuiColorInput
           value={colorValue}
@@ -53,7 +53,7 @@ export const LabelPopup: React.FC = () => {
       </div>
 
       <div className="flex justify-end space-x-2 pt-2">
-        <button className="w-[88px] bg-white px-3 py-1 text-[20px]">
+        <button className="w-[88px] bg-gray-200 px-3 py-1 text-[20px]">
           Cancel
         </button>
         <button className="w-[88px] bg-gray-500 text-white px-3 py-1 text-[20px]">
