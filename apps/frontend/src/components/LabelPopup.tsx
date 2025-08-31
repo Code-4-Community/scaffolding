a578c98d3816586a5fbf672a6f76121e94a85b3a
Dalton Burkhart <daltonburkhart.75@gmail.com>
2025-08-31 13:19:39 -0400

    Made updates based on Figma

--- apps/frontend/src/components/LabelPopup.tsx
+++ apps/frontend/src/components/LabelPopup.tsx
@@ -15,21 +15,21 @@ export const LabelPopup: React.FC = () => {
   return (
     <form
       onSubmit={handleSubmit}
-      className="w-[338px] p-6 border-2 border-black bg-gray-100 space-y-2 text-[28px]"
+      className="w-[278px] px-4 py-1 border-2 rounded-xl bg-gray-100 space-y-2 text-[18px] text-[#424242]"
     >
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-1">
         <label className="font-bold">Name</label>
         <input
           type="text"
           placeholder="Label.."
           value={name}
           onChange={(e) => setName(e.target.value)}
-          className="w-full px-3 text-[22px]"
+          className="w-[211px] px-3 text-[14px]"
           required
         />
       </div>
 
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-1">
         <label className="font-bold">
           Description <span className="text-xs">(optional)</span>
         </label>
@@ -38,7 +38,7 @@ export const LabelPopup: React.FC = () => {
           placeholder="Description.."
           value={description}
           onChange={(e) => setDescription(e.target.value)}
-          className="w-full px-3 text-[22px]"
+          className="w-[211px] px-3 text-[14px]"
           required
         />
       </div>
@@ -52,11 +52,11 @@ export const LabelPopup: React.FC = () => {
         />
       </div>
 
-      <div className="flex justify-end space-x-2 pt-2">
-        <button className="w-[88px] bg-gray-200 px-3 py-1 text-[20px]">
+      <div className="flex justify-end space-x-2 py-2">
+        <button className="w-[60px] bg-gray-200 px-3 py-1 text-[12px]">
           Cancel
         </button>
-        <button className="w-[88px] bg-gray-500 text-white px-3 py-1 text-[20px]">
+        <button className="w-[60px] bg-gray-500 text-white px-3 py-1 text-[12px]">
           Create
         </button>
       </div>
