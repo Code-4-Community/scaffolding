239614ad8c6a79ab0ae712226fad36ad7a147974
Rayyan Mridha <66543565+rayyanmridha@users.noreply.github.com>
2025-11-13 20:01:00 -0500

    fix up sole card

--- apps/frontend/src/containers/root.tsx
+++ apps/frontend/src/containers/root.tsx
@@ -1,5 +1,18 @@
+import PendingReviewCard from '@components/PendingReviewCard';
+import { TimeIcon } from '@chakra-ui/icons';
+
 const Root: React.FC = () => {
-  return <>Welcome to scaffolding!</>;
+  return (
+    <>
+      Welcome to scaffolding!
+      <PendingReviewCard
+        title="Total Applications"
+        value={298}
+        description="All time submissions"
+        icon={TimeIcon}
+      />
+    </>
+  );
 };
 
 export default Root;
--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,18 +1,13 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import AdminHomePage from 'admin/AdminHomePage';
-import { ChakraProvider, createSystem, defaultConfig } from '@chakra-ui/react';
 
 import App from './app';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
-const system = createSystem(defaultConfig);
 root.render(
   <StrictMode>
-    <ChakraProvider value={system}>
-      <AdminHomePage />
-    </ChakraProvider>
+    <App />
   </StrictMode>,
 );
