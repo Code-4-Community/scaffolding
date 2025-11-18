e27eb13887be2916cba6c83c64cd5cd5b93730e9
bodhiYG <geslani.b@northeastern.edu>
2025-11-18 18:53:47 -0500

    Approved Card

--- apps/frontend/src/containers/root.tsx
+++ apps/frontend/src/containers/root.tsx
@@ -1,5 +1,18 @@
+import { ApprovedCard } from '@components/ApprovedCard';
+import { IoCheckmarkCircleOutline } from 'react-icons/io5';
+
 const Root: React.FC = () => {
-  return <>Welcome to scaffolding!</>;
+  return (
+    <>
+      Welcome to scaffolding!
+      <ApprovedCard
+        title="Approved"
+        count={102}
+        description="Active volunteers"
+        icon={<IoCheckmarkCircleOutline />}
+      />
+    </>
+  );
 };
 
 export default Root;
