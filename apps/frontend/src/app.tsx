2f855c6c62b208fb671994ac7b841a71c53466f1
Sam Nie <147653722+SamNie2027@users.noreply.github.com>
2025-12-18 23:11:21 -0500

    Removal of unnecessary initialization project components
    Removal of unnecessary initialization project components
    App.spec.tsx - This contains an example UI test that is irrelevant to us right now, and if we do implement UI tests in the future we are likely not going to be using render to test it.
    Testing page - this is just an unnecessary page returning just the text "I am a test page" that had a route so I removed that too.

--- apps/frontend/src/app.tsx
+++ apps/frontend/src/app.tsx
@@ -5,17 +5,12 @@ import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
 import apiClient from '@api/apiClient';
 import Root from '@containers/root';
 import NotFound from '@containers/404';
-import Test from '@containers/test';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
     errorElement: <NotFound />,
-  },
-  {
-    path: '/test',
-    element: <Test />,
   },
 ]);
 
