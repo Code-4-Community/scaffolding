e3c72e8038b22324780f9085abeb48b1fd080d2e
Sam Nie <147653722+SamNie2027@users.noreply.github.com>
2026-01-13 22:24:09 -0500

    Changing SearchBar to use standard React practices

--- apps/frontend/src/components/SearchBar.tsx
+++ apps/frontend/src/components/SearchBar.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import { Flex, Input } from '@chakra-ui/react';
 import { IoSearch } from 'react-icons/io5';
 
-interface SearchBarProps {
+export type SearchBarProps = {
   placeholder?: string;
   value?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-}
+};
 
-const SearchBar: React.FC<SearchBarProps> = ({
+export default function SearchBar({
   placeholder = 'Search by Keyword',
   value,
   onChange,
-}) => {
+}: SearchBarProps) {
   return (
     <Flex align="center" bg="#204AA0" width="100%" borderRadius="4px" px="8px">
       <IoSearch color="white" size="16px" />
@@ -33,6 +33,4 @@ const SearchBar: React.FC<SearchBarProps> = ({
       />
     </Flex>
   );
-};
-
-export default SearchBar;
+}
