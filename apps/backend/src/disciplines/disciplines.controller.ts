69bb7bd24d76014da54e67fe7572f61e5154a212
Sam Nie <147653722+SamNie2027@users.noreply.github.com>
2026-01-19 10:41:14 -0500

    Adding documentation and minor syntactical changes.

--- apps/backend/src/disciplines/disciplines.controller.ts
+++ apps/backend/src/disciplines/disciplines.controller.ts
@@ -11,6 +11,7 @@ import { AuthGuard } from '@nestjs/passport';
 import { CurrentUserInterceptor } from '../interceptors/current-user.interceptor';
 import { DisciplinesService } from './disciplines.service';
 import { CreateDisciplineRequestDto } from './dto/create-discipline.request.dto';
+import { Discipline } from './disciplines.entity';
 
 /**
  * Controller to expose callable HTTP endpoints to interface,
@@ -21,18 +22,35 @@ import { CreateDisciplineRequestDto } from './dto/create-discipline.request.dto'
 @UseGuards(AuthGuard('jwt'))
 export class DisciplinesController {
   constructor(private disciplinesService: DisciplinesService) {}
+
+  /**
+   * Exposes an endpoint to return a list of all disciplines
+   * @returns a list of all disciplines
+   */
   @Get()
-  async getAll() {
+  async getAll(): Promise<Discipline[]> {
     return this.disciplinesService.findAll();
   }
 
+  /**
+   * Exposes an endpoint to return a discipline by id
+   * @param id the id of the discipline to return
+   * @returns the discipline with the corresponding id
+   */
   @Get(':id')
-  async getOne(@Param('id') id: string) {
+  async getOne(@Param('id') id: string): Promise<Discipline> {
     return this.disciplinesService.findOne(Number(id));
   }
 
+  /**
+   * Exposes an endpoint to create a new discipline
+   * @param createDto object containing necessary info to create an new discipline, like the name
+   * @returns the new discipline
+   */
   @Post()
-  async create(@Body() createDto: CreateDisciplineRequestDto) {
+  async create(
+    @Body() createDto: CreateDisciplineRequestDto,
+  ): Promise<Discipline> {
     return this.disciplinesService.create(createDto);
   }
 }
--- apps/backend/src/disciplines/disciplines.service.ts
+++ apps/backend/src/disciplines/disciplines.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Discipline } from './disciplines.entity';
+import { CreateDisciplineRequestDto } from './dto/create-discipline.request.dto';
 
 /**
  * Service to interface with the disciplines repository.
@@ -13,17 +14,29 @@ export class DisciplinesService {
     private disciplinesRepository: Repository<Discipline>,
   ) {}
 
+  /**
+   * Returns a list of all disciplines in the repository
+   * @returns a list of all disciplines in the repository
+   */
   async findAll(): Promise<Discipline[]> {
     return this.disciplinesRepository.find();
   }
 
+  /**
+   * Returns a discipline from the repository with the respective id
+   * @param id the id corresponding to the desired discipline
+   * @returns a discipline from the repository with the respective id
+   */
   async findOne(id: number): Promise<Discipline | null> {
     return this.disciplinesRepository.findOneBy({ id });
   }
 
-  async create(
-    createDto: import('./dto/create-discipline.request.dto').CreateDisciplineRequestDto,
-  ): Promise<Discipline> {
+  /**
+   * Creates a discipline with the requested fields
+   * @param createDto the requested fields for the new discipline to have
+   * @returns the new discipline
+   */
+  async create(createDto: CreateDisciplineRequestDto): Promise<Discipline> {
     const discipline = this.disciplinesRepository.create(createDto);
     return this.disciplinesRepository.save(discipline);
   }
