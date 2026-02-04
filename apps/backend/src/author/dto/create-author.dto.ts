df131e0d8b8c29772449929d8c37074b0ac292df
josieek <kummer.j@northeastern.edu>
2026-02-04 13:38:03 -0500

    migrations changed to compile js instead of ts. added author dto and tested in postman

--- apps/backend/src/author/dto/create-author.dto.ts
+++ apps/backend/src/author/dto/create-author.dto.ts
@@ -1,1 +1,15 @@
-export class CreateAuthorDto {}
+import { IsString, IsInt, IsOptional, MinLength } from 'class-validator';
+
+export class CreateAuthorDto {
+  @IsString()
+  @MinLength(1)
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  bio?: string;
+
+  @IsOptional()
+  @IsInt()
+  grade?: number;
+}
