0dfcd59274dcaa77b6f0b1c7fde896f811c21783
chnnick <nickchenschool@gmail.com>
2026-02-25 14:26:19 -0500

    new columns, renamed proposedDate, added actualStartDate

--- apps/frontend/src/components/ApplicationTable.tsx
+++ apps/frontend/src/components/ApplicationTable.tsx
@@ -2,7 +2,8 @@ import { Table } from '@chakra-ui/react';
 
 const COLUMNS = [
   'Name',
-  'Start Date',
+  'Proposed Date',
+  'Actual Start Date',
   'Experience Type',
   'Discipline',
   'Discipline Admin Name',
@@ -13,7 +14,8 @@ const APPLICATIONS = [
   {
     id: '1',
     name: 'Firstname Lastname',
-    startDate: '01-01-2026',
+    proposedDate: '01-01-2026',
+    actualStartDate: '01-07-2026',
     experienceType: 'Volunteer',
     discipline: 'Nursing',
     disciplineAdminName: 'Firstname Lastname',
@@ -22,7 +24,8 @@ const APPLICATIONS = [
   {
     id: '2',
     name: 'Firstname Lastname',
-    startDate: '01-01-2026',
+    proposedDate: '01-01-2026',
+    actualStartDate: '01-06-2026',
     experienceType: 'Volunteer',
     discipline: 'Nursing',
     disciplineAdminName: 'Firstname Lastname',
@@ -31,7 +34,8 @@ const APPLICATIONS = [
   {
     id: '3',
     name: 'Firstname Lastname',
-    startDate: '01-01-2026',
+    proposedDate: '01-01-2026',
+    actualStartDate: '01-05-2026',
     experienceType: 'Volunteer',
     discipline: 'Nursing',
     disciplineAdminName: 'Firstname Lastname',
@@ -40,7 +44,8 @@ const APPLICATIONS = [
   {
     id: '4',
     name: 'Firstname Lastname',
-    startDate: '01-01-2026',
+    proposedDate: '01-01-2026',
+    actualStartDate: '01-03-2026',
     experienceType: 'Volunteer',
     discipline: 'Nursing',
     disciplineAdminName: 'Firstname Lastname',
@@ -49,7 +54,8 @@ const APPLICATIONS = [
   {
     id: '5',
     name: 'Firstname Lastname',
-    startDate: '01-01-2026',
+    proposedDate: '01-01-2026',
+    actualStartDate: '01-02-2026',
     experienceType: 'Volunteer',
     discipline: 'Nursing',
     disciplineAdminName: 'Firstname Lastname',
@@ -77,7 +83,8 @@ export const ApplicationTable: React.FC = () => {
         {APPLICATIONS.map((application) => (
           <Table.Row key={application.id}>
             <Table.Cell>{application.name}</Table.Cell>
-            <Table.Cell>{application.startDate}</Table.Cell>
+            <Table.Cell>{application.proposedDate}</Table.Cell>
+            <Table.Cell>{application.actualStartDate}</Table.Cell>
             <Table.Cell>{application.experienceType}</Table.Cell>
             <Table.Cell>{application.discipline}</Table.Cell>
             <Table.Cell>{application.disciplineAdminName}</Table.Cell>
