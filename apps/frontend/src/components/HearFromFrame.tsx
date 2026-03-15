bf052ceaf961e8d50f969d8239f760b9fac69620
chnnick <nickchenschool@gmail.com>
2026-03-14 21:58:52 -0400

    abstracted out frames

--- apps/frontend/src/components/HearFromFrame.tsx
+++ apps/frontend/src/components/HearFromFrame.tsx
@@ -1,15 +1,22 @@
 import { Box, Button, Flex, Heading, SimpleGrid } from '@chakra-ui/react';
 
+interface QuestionFrameProps {
+  question: string;
+  answers: string[];
+}
+
 // TODO: Add options from shared source, currently hardcoded
-export const HearFromFrame: React.FC<{ sources: string[] }> = ({ sources }) => {
+export const HearFromFrame: React.FC<{ frameProps: QuestionFrameProps }> = ({
+  frameProps,
+}) => {
   return (
     <Box borderWidth="1px" borderRadius="lg" p={6} bg="white">
       <Flex gap={4} alignItems="center" flexWrap="wrap">
         <Heading as="h2" size="md" flexShrink={0}>
-          How did you hear about us?
+          {frameProps.question}
         </Heading>
         <SimpleGrid columns={{ base: 1, sm: 2, md: 3 }} gap={2}>
-          {sources.map((source) => (
+          {frameProps.answers.map((answer) => (
             <Button
               borderRadius="full"
               px={4}
@@ -20,9 +27,9 @@ export const HearFromFrame: React.FC<{ sources: string[] }> = ({ sources }) => {
               color="white"
               fontFamily="Poppins, sans-serif"
               border="1px solid #013594"
-              key={source}
+              key={answer}
             >
-              {source}
+              {answer}
             </Button>
           ))}
         </SimpleGrid>
