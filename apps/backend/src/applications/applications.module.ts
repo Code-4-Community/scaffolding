c48bfeb722a465c2088708b2cfe1d6bfe2186a83
chnnick <nickchenschool@gmail.com>
2026-03-31 21:05:42 -0400

    bring in users and email utility modules

--- apps/backend/src/applications/applications.module.ts
+++ apps/backend/src/applications/applications.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApplicationsController } from './applications.controller';
 import { ApplicationsService } from './applications.service';
 import { Application } from './application.entity';
+import { UtilModule } from '../util/util.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Application])],
+  imports: [TypeOrmModule.forFeature([Application]), UtilModule, UsersModule],
   controllers: [ApplicationsController],
   providers: [ApplicationsService],
 })
