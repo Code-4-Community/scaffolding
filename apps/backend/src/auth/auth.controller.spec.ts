5d14f5c8f10fc3f1fc033f6d8051a03cae3ee520
Sam Nie <147653722+SamNie2027@users.noreply.github.com>
2026-05-05 10:26:46 -0400

    Adding basic CICD, commenting out old CICD, disabling nx cloud (#111)
    * Adding basic CICD, commenting out old CICD, disabling nx cloud
    
    * Changed runner
    
    * Implementing feedback
    
    * Update lint.yml
    
    * remove task, update readme
    
    * test fetch depth fix
    
    * test commit
    
    * bug fix
    
    * Update auth.controller.spec.ts
    
    * fix tests
    
    * Trailing new line
    
    * Update yarn.lock
    
    * Update yarn.lock
    
    ---------
    
    Co-authored-by: Max Norman <27299284+maxn990@users.noreply.github.com>
    Co-authored-by: Max Norman <maxn990@icloud.com>

--- apps/backend/src/auth/auth.controller.spec.ts
+++ apps/backend/src/auth/auth.controller.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { UsersService } from '../users/users.service';
+import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const module = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: { login: jest.fn(), validateUser: jest.fn() },
+        },
+        {
+          provide: UsersService,
+          useValue: { findOne: jest.fn(), create: jest.fn() },
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
