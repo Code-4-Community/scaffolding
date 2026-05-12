c9411ab53a4aea37ebfe1c0617010f368a54238f
chnnick <nickchenschool@gmail.com>
2026-05-11 17:35:05 -0700

    inline comments as directions

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -22,18 +22,20 @@ async function bootstrap() {
   );
 
   if (process.env.SWAGGER_ENABLED === 'TRUE') {
-    // Update with your app's specific name, description, tags, and version
     const config = new DocumentBuilder()
+      // Update with your app's specific name
       .setTitle('[YOUR_APP_NAME] API Docs')
+      // Update with your app's specific description
       .setDescription('Documentation for [YOUR_APP_NAME] API routes')
+      // Add bearer auth for protected routes (Use @ApiBearerAuth() decorator on the controller for the routes that should be protected)
       .addBearerAuth()
+      // Example tag for a users controller
       .addTag('Users', 'Operations on users')
-      .addTag('Auth', 'Operations for authentication')
       // Update with your app's version
       .setVersion('1.0')
       .build();
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
+    const document = SwaggerModule.createDocument(app, config); // Create the Swagger document
+    SwaggerModule.setup('api', app, document); // Setup the Swagger module
     Logger.log(
       `😎 Swagger is enabled and will be available at: http://localhost:${
         process.env.PORT || 3000
