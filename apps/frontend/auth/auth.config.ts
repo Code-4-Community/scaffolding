94a3b9ea0712148f9476d561c81a07126bcaa779
chnnick <nickchenschool@gmail.com>
2026-05-15 15:39:58 -0700

    correctly importing env variables, type guards for each value so configure runs correctly with cognito values as strings.

--- apps/frontend/auth/auth.config.ts
+++ apps/frontend/auth/auth.config.ts
@@ -1,10 +1,10 @@
 import { Amplify } from 'aws-amplify';
 
-const userPoolId = import.meta.env.VITE_COGNITO_USER_POOL_ID;
-const userPoolClientId = import.meta.env.VITE_COGNITO_USER_POOL_CLIENT_ID;
+const userPoolId = process.env.VITE_COGNITO_USER_POOL_ID;
+const userPoolClientId = process.env.VITE_COGNITO_USER_POOL_CLIENT_ID;
 
 // Fail out if the values are not set in the environment variables even if they exist
-function isNonEmptyEnv(value: string | undefined): boolean {
+function isNonEmptyEnv(value: string | undefined): value is string {
   if (value === undefined || value === '') {
     return false;
   }
@@ -20,7 +20,10 @@ export const cognitoInformationPresent =
 
 // Configure amplify with cognito if the information is present in the environment variables
 export function configureAmplify(): void {
-  if (!cognitoInformationPresent) {
+  if (!isNonEmptyEnv(userPoolId)) {
+    return;
+  }
+  if (!isNonEmptyEnv(userPoolClientId)) {
     return;
   }
 
