c2226130340d87060c29b54c930b96cdea595b54
chnnick <nickchenschool@gmail.com>
2026-05-20 12:23:04 -0700

    cognito config + updated types to allow for both id and access tokens

--- apps/backend/src/aws/cognito/cognito.types.ts
+++ apps/backend/src/aws/cognito/cognito.types.ts
@@ -1,12 +1,14 @@
-// Type for the payload of our Cognito JWT token: https://docs.aws.amazon.com/cognito/latest/developerguide/amazon-cognito-user-pools-using-the-id-token.html
+// Cognito ID token: https://docs.aws.amazon.com/cognito/latest/developerguide/amazon-cognito-user-pools-using-the-id-token.html
+// Cognito access token: https://docs.aws.amazon.com/cognito/latest/developerguide/amazon-cognito-user-pools-using-the-access-token.html
 
 export interface CognitoJwtPayload {
   sub: string; // Subject (unique identifier for the user)
-  'cognito:groups': string[]; // Groups (ex: ['admin', 'user'])
+  'cognito:groups'?: string[]; // Groups (ex: ['admin', 'user'])
   iss: string; // Issuer (ex: https://cognito-idp.<Region>.amazonaws.com/<your user pool ID>)
-  token_use: 'id'; // Token use (Amplify sends ID tokens to identify the user)
-  aud: string; // User pool app client that authenticated the user (ex: <your client ID>)
+  token_use?: 'id' | 'access'; // Token use (ID or access token from Cognito)
+  aud?: string; // Audience: Client ID this token is intended for (ex: <your client ID>)
+  client_id?: string; // Client ID used during authentication (ex: <your client ID>)
   exp: number; // Expiration time (Unix timestamp)
   iat: number; // Issued at time (Unix timestamp)
-  email: string; // Email (ex: test@example.com)
+  email?: string; // Email (ex: test@example.com)
 }
