146b2719b47330f9e6fa0b9d748525eda41e4a51
chnnick <nickchenschool@gmail.com>
2026-06-02 13:51:18 -0700

    get rid of VITE specific cognito variables in .env instead have them autogenerated in vite.config, also set the project root to allow frontend access to root /.env file.

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,46 +1,56 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import path from 'path';
 
-export default defineConfig({
-  cacheDir: '../../node_modules/.vite/frontend',
+// Assumes this file is in /apps/frontend/vite.config.ts and env variables are set in /.env
+const workspaceRoot = path.resolve(__dirname, '../..');
 
-  server: {
-    port: 4200,
-    host: 'localhost',
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, workspaceRoot, '');
+  process.env.VITE_COGNITO_USER_POOL_ID = env.COGNITO_USER_POOL_ID ?? '';
+  process.env.VITE_COGNITO_USER_POOL_CLIENT_ID = env.COGNITO_CLIENT_ID ?? '';
 
-  preview: {
-    port: 4300,
-    host: 'localhost',
-  },
+  return {
+    cacheDir: '../../node_modules/.vite/frontend',
+    envDir: workspaceRoot,
 
-  plugins: [react(), nxViteTsPaths()],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-
-  test: {
-    globals: true,
-    cache: {
-      dir: '../../node_modules/.vitest',
+    server: {
+      port: 4200,
+      host: 'localhost',
     },
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-  },
 
-  resolve: {
-    alias: {
-      '@api': path.resolve(__dirname, './src/api'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@containers': path.resolve(__dirname, './src/containers'),
-      '@public': path.resolve(__dirname, './public'),
-      '@shared': path.resolve(__dirname, '../../shared'),
-      '@utils': path.resolve(__dirname, './src/utils'),
+    preview: {
+      port: 4300,
+      host: 'localhost',
     },
-  },
+
+    plugins: [react(), nxViteTsPaths()],
+
+    // Uncomment this if you are using workers.
+    // worker: {
+    //  plugins: [ nxViteTsPaths() ],
+    // },
+
+    test: {
+      globals: true,
+      cache: {
+        dir: '../../node_modules/.vitest',
+      },
+      environment: 'jsdom',
+      include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    },
+
+    resolve: {
+      alias: {
+        '@api': path.resolve(__dirname, './src/api'),
+        '@components': path.resolve(__dirname, './src/components'),
+        '@containers': path.resolve(__dirname, './src/containers'),
+        '@public': path.resolve(__dirname, './public'),
+        '@shared': path.resolve(__dirname, '../../shared'),
+        '@utils': path.resolve(__dirname, './src/utils'),
+      },
+    },
+  };
 });
