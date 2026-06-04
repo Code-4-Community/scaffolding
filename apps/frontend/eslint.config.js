839fcba5d7a6ead02e9636d3a76f50d85ca00450
Max Norman <27299284+maxn990@users.noreply.github.com>
2026-06-04 12:43:54 -0400

    fix tsconfig and vite

---
+++ apps/frontend/eslint.config.js
@@ -0,0 +1,14 @@
+const { FlatCompat } = require('@eslint/eslintrc');
+const baseConfig = require('../../eslint.config.js');
+
+const compat = new FlatCompat({ baseDirectory: __dirname });
+
+module.exports = [
+  { ignores: ['eslint.config.js'] },
+  ...baseConfig,
+  ...compat.extends('plugin:@nx/react'),
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    rules: {},
+  },
+];
---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+const { FlatCompat } = require('@eslint/eslintrc');
+const js = require('@eslint/js');
+const nxPlugin = require('@nx/eslint-plugin');
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+});
+
+module.exports = [
+  {
+    plugins: { '@nx': nxPlugin },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    ignores: ['**/eslint.config.js'],
+    rules: {
+      '@nx/enforce-module-boundaries': [
+        'error',
+        {
+          enforceBuildableLibDependency: true,
+          allow: [],
+          depConstraints: [{ sourceTag: '*', onlyDependOnLibsWithTags: ['*'] }],
+        },
+      ],
+    },
+  },
+  ...compat.extends('plugin:@nx/typescript').map(config => ({
+    ...config,
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {
+      ...config.rules,
+      '@typescript-eslint/no-unused-vars': ['warn', {}],
+    },
+  })),
+  ...compat.extends('plugin:@nx/javascript').map(config => ({
+    ...config,
+    files: ['**/*.js', '**/*.jsx'],
+  })),
+];
