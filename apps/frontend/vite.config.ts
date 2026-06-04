0f1d1952566fd409b2525839fb270b7d7d9d6cb5
Max Norman <27299284+maxn990@users.noreply.github.com>
2026-06-04 13:05:26 -0400

    vite config

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -5,6 +5,7 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import path from 'path';
 
 export default defineConfig({
+  root: __dirname,
   cacheDir: '../../node_modules/.vite/frontend',
 
   server: {
